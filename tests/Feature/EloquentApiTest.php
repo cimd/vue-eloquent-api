8317646e674cea57ed86d3c65f10383c8bf58a59
Christian D'Aquino <christian.daquino@gmail.com>
2023-04-20 16:37:11 +0800

    Update EloquentApiTest.php

--- tests/Feature/EloquentApiTest.php
+++ tests/Feature/EloquentApiTest.php
@@ -1,13 +1,14 @@
 <?php
 
 test('filters', function () {
-    $response = $this->getJson('/api/users?filter[id]=1');
-
+    $response = $this->getJson('/api/posts?filter[author_id]=1');
+    var_dump(json_encode($response));
+    ob_flush();
     expect(true)->toBeTrue();
 });
 
 test('relations', function () {
-    $response = $this->getJson('/api/users?include=posts');
+    $response = $this->getJson('/api/posts?include=posts');
 
     expect(true)->toBeTrue();
 });
