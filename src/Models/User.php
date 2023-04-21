72a22070fb05748d9f80387c5213986dfc2a450c
Christian D'Aquino <christian.daquino@gmail.com>
2023-04-21 13:19:31 +0800

    cleanup

--- src/Models/User.php
+++ src/Models/User.php
@@ -2,17 +2,14 @@
 
 namespace Konnec\VueEloquentApi\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 use Konnec\VueEloquentApi\Filters\WhereEqual;
 use Konnec\VueEloquentApi\Traits\EloquentApi;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
     use EloquentApi;
 
     /**
